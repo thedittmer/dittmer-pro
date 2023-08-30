ad51d884a191f0b7c40fed5950dc85ec1adabf7d
Jason Dittmer <jason@zrimarketing.com>
2023-08-30 17:36:59 -0500

    info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -45,23 +45,27 @@
 						</button>
 					{/if}
 				</div>
-				<p class="my-3 text-2xl text-center dark:text-white">Hi, I&#x27;m Jason 🤘</p>
-				<h2
-					class="max-w-3xl py-2 mx-auto text-4xl font-bold text-center text-gray-800 dark:text-white"
-				>
-					Dittmer Internet Pro
-				</h2>
-				<div class="flex items-center justify-center mt-4 dark:text-white">
-					<a
-						href="https://www.linkedin.com/in/jason-dittmer-04773223/"
-						target="_blank"
-						class="mx-4 underline">LinkedIn</a
+				<div class="max-w-prose mx-auto text-center pt-4">
+					<p class="my-3 text-2xl text-center dark:text-white">Hi, I&#x27;m Jason Dittmer 🤘</p>
+					<h2
+						class="max-w-3xl py-2 mx-auto text-xl font-bold text-center text-gray-800 dark:text-white"
 					>
-					<a href="https://twitter.com/thedittmer" target="_blank" class="mx-4 underline">Twitter</a
-					>
-					<a href="https://www.facebook.com/thedittmer/" target="_blank" class="mx-4 underline"
-						>Facebook</a
-					>
+						Web Developer & Software Engineer
+					</h2>
+					<p class="text-gray-800 dark:text-white">I can help you with your next project.</p>
+					<div class="flex items-center justify-center mt-4 dark:text-white">
+						<a
+							href="https://www.linkedin.com/in/jason-dittmer-04773223/"
+							target="_blank"
+							class="mx-4 underline">LinkedIn</a
+						>
+						<a href="https://twitter.com/thedittmer" target="_blank" class="mx-4 underline"
+							>Twitter</a
+						>
+						<a href="https://www.facebook.com/thedittmer/" target="_blank" class="mx-4 underline"
+							>Facebook</a
+						>
+					</div>
 				</div>
 			</div>
 		</div>
