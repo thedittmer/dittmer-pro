7668b98092eff27d8827d75e87f23fcd6d0ea939
Jason Dittmer <jason@zrimarketing.com>
2023-08-30 16:19:06 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+
+<main class="relative h-screen overflow-hidden font-mono bg-white dark:bg-gray-800">
+	<header class="z-30 flex items-center w-full h-24 sm:h-32" />
+	<div class="relative z-20 flex items-center">
+		<div class="container relative flex flex-col items-center justify-between px-6 py-4 mx-auto">
+			<div class="flex flex-col">
+				<img alt="Jason Dittmer" src="/Jason Dittmer.webp" class="mx-auto rounded-full w-70" />
+				<p class="my-6 text-3xl text-center dark:text-white">Hi, I&#x27;m Jason 🤘</p>
+				<h2
+					class="max-w-3xl py-2 mx-auto text-5xl font-bold text-center text-gray-800 md:text-6xl dark:text-white"
+				>
+					Dittmer Internet Pro
+				</h2>
+				<div class="flex items-center justify-center mt-4 dark:text-white">
+					<a
+						href="https://www.linkedin.com/in/jason-dittmer-04773223/"
+						target="_blank"
+						class="mx-4 underline">LinkedIn</a
+					>
+					<a href="https://twitter.com/thedittmer" target="_blank" class="mx-4 underline">Twitter</a
+					>
+					<a href="https://www.facebook.com/thedittmer/" target="_blank" class="mx-4 underline"
+						>Facebook</a
+					>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
