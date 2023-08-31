42deefe4eabe67a97940021f34869acbfc1bb28f
Jason Dittmer <jason@zrimarketing.com>
2023-08-31 06:57:32 -0500

    dittmers chat room

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -12,14 +12,14 @@
 </script>
 
 <svelte:head>
-	<title>Jason's Chat Room</title>
+	<title>Dittmer's Chat Room</title>
 </svelte:head>
 
 <main class="p-5">
 	<a href="/" class="underline dark:text-green-500 0">🔙 Back to Home</a>
 	<div class="max-w-prose dark:text-white pb-10">
 		<h1 class="pb-2 pt-7 text-lg md:text-xl font-bold text-gray-800 dark:text-white">
-			Jason's Chat Room
+			Dittmer's Chat Room
 		</h1>
 		<p class="pb-2">Thanks for stoping by. This is just a chat room for fun.</p>
 		<p>If we've worked together before or you know me, feel free to send a message.</p>
