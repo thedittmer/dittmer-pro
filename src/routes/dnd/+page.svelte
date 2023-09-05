a37f61b8c33cba3f9cd37dbcf392af4a99294906
Jason Dittmer <jason@zrimarketing.com>
2023-09-05 18:23:41 -0500

    dnd

--- src/routes/dnd/+page.svelte
+++ src/routes/dnd/+page.svelte
@@ -1,23 +1,33 @@
 <script lang="ts">
-	import { dndzone } from 'svelte-dnd-action';
+	import { dndzone, overrideItemIdKeyNameBeforeInitialisingDndZones } from 'svelte-dnd-action';
 	import { flip } from 'svelte/animate';
+	import { currentUser, getAvatarUrl, pb } from '$lib/pocketbase';
+	import { onMount, onDestroy } from 'svelte';
+	import { fly, fade } from 'svelte/transition';
+
+	overrideItemIdKeyNameBeforeInitialisingDndZones("order");
 
 	const flipDurationMs = 200;
 	function handleSort(e: CustomEvent<DndEvent>) {
-		items = e.detail.items as { id: number; title: string }[];
+		items = e.detail.items as { order: number; title: string }[];
 	}
 
-	let items = [
-		{ id: 1, title: 'I' },
-		{ id: 2, title: 'Am' },
-		{ id: 3, title: 'Yoda' }
-	];
+	let items: any[] = [];
+
+	onMount(async () => {
+		// Get initial todos
+		const resultList = await pb.collection('todos').getList(1, 50, {
+			sort: '-created',
+			expand: 'user'
+		});
+		items = resultList.items;
+	});
 </script>
 
 <section use:dndzone={{ items, flipDurationMs }} on:consider={handleSort} on:finalize={handleSort}>
-	{#each items as item (item.id)}
+	{#each items as item (item.order)}
 		<div animate:flip={{ duration: flipDurationMs }} class="dark:text-white pb-10">
-			{item.title}
+			{item.title} - {item.id}
 		</div>
 	{/each}
 </section>
