684eb0456538352292e863420049497f0ece6daf
Jason Dittmer <jason@zrimarketing.com>
2024-04-27 09:36:17 -0500

    house for sale

---
+++ src/routes/818-riverside-dr-neosho-mo/+page.server.js
@@ -0,0 +1,19 @@
+import { redirect, error } from '@sveltejs/kit';
+
+export async function load({ locals }) {
+
+	let route;
+	try {
+		route = await locals.pb.collection('pages').getFirstListItem(`slug="818-riverside-dr-neosho-mo"`);
+		console.log('route', route);
+	} catch (err) {
+		throw error(404);
+	}
+
+	return {
+		route,
+	};
+}
+
+export const trailingSlash = 'always';
+export const prerender = true;
