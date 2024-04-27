101a887f78e3fc9ae37ff8b3095efcb498cd5ba0
Jason Dittmer <jason@zrimarketing.com>
2024-04-27 11:54:37 -0500

    fathom

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
 <script>
 	import '../app.css';
+	import { PUBLIC_FATHOM_ID, PUBLIC_FATHOM_URL } from '$env/static/public';
+	import * as Fathom from 'fathom-client';
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
+	import { navigating, page } from '$app/stores';
+
+	if (PUBLIC_FATHOM_ID !== 'DEV') {
+		onMount(async () => {
+			Fathom.load(PUBLIC_FATHOM_ID, {
+				url: PUBLIC_FATHOM_URL
+			});
+		});
+	}
+
+	// track a page view when the pathname changes
+	$: $page.url.pathname, browser && Fathom.trackPageview();
 </script>
 
 <slot />
