73af2975be59e378f14137c6e0180f4880b07123
Jason Dittmer <thedittmer@gmail.com>
2026-02-14 18:32:23 -0600

    Update +page.svelte

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,11 +1,9 @@
 <iframe
-	title="Neosho Map"
 	src="https://dev.neosho.sale/embed/map"
+	title="Neosho Garage Sale Map"
 	width="100%"
 	height="500"
 	frameborder="0"
 	style="border:0;border-radius:8px;"
 	allowfullscreen
-/>Ï
-
-<h1>Test</h1>
+/>
