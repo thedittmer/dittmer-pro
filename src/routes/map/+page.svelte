a9ecda30245bda406ff0c0496a23b608cbccab96
Jason Dittmer <thedittmer@gmail.com>
2026-02-15 05:19:56 -0600

    Update +page.svelte

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,8 +1,7 @@
-<iframe
-	src="https://www.neosho.sale/embed/map"
-	width="100%"
-	height="500"
-	frameborder="0"
-	style="border:0;border-radius:8px;"
-	allowfullscreen
-/>
+<div style="position:relative;width:100%;padding-top:75%;min-height:400px;">
+	<iframe
+		src="https://www.neosho.sale/embed/map"
+		style="position:absolute;top:0;left:0;width:100%;height:100%;border:0;border-radius:8px;"
+		allowfullscreen
+	/>
+</div>
